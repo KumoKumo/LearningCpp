208e8c39665151586ab28819c05cd161384f3f0a
KumoKumo <kuroryouji@gmail.com>
2021-01-06 22:57:15 +0900

    Add Condition and Branches example

--- LearningCpp/Main.cpp
+++ LearningCpp/Main.cpp
@@ -1,8 +1,21 @@
 #include<iostream>
 #include "Log.h"
 
-int main() 
+int main()
 {
-	Log("Hello World!");
+	int x = 6;
+	bool comparisonResult = x == 5;
+	if (!comparisonResult)
+		Log("It's 5!");
+
+	const char* ptr = "Hello";
+	if (ptr)
+		Log("Not null!");
+	
+	const char* ptrnull = nullptr;
+	if (!ptrnull)
+		Log("Null");
+
+
 	std::cin.get();
 }
