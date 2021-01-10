48bef0d97699c2379987a7f918c7dd4ef43ae560
KumoKumo <kuroryouji@gmail.com>
2021-01-10 16:52:32 +0900

    Add Loop Example

--- LearningCpp/src/Main.cpp
+++ LearningCpp/src/Main.cpp
@@ -1,8 +1,26 @@
 #include<iostream>
 #include "../header/Log.h"
+#include "Main.h"
 
-int main()
+void DoingLoopExample()
 {
+	Log("===========================================");
+	Log("Doing Loop Example: ");
+	int i = 0;
+	bool condition = true;
+	for (; condition; )
+	{
+		Log("Hello World!");
+		i++;
+		if (!(i < 5))
+			condition = false;
+	}
+}
+
+void DoingConditionExample()
+{
+	Log("===========================================");
+	Log("Doing ConditionExample: ");
 	int x = 6;
 	bool comparisonResult = x == 5;
 	if (!comparisonResult)
@@ -11,11 +29,17 @@ int main()
 	const char* ptr = "Hello";
 	if (ptr)
 		Log("Not null!");
-	
+
 	const char* ptrnull = nullptr;
 	if (!ptrnull)
 		Log("Null");
+}
 
+int main()
+{
+	DoingConditionExample();
+
+	DoingLoopExample();
 
 	std::cin.get();
 }
