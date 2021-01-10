9ed8436efd2a7da7406fd7d1892d347b554c0fd6
KumoKumo <kuroryouji@gmail.com>
2021-01-10 23:55:53 +0900

    Add Class Example

--- LearningCpp/src/Main.cpp
+++ LearningCpp/src/Main.cpp
@@ -1,11 +1,28 @@
 #include "../header/Topics.h"
 
+class Player
+{
+public:
+	int x, y;
+	int speed;
+
+	void Move(int xAmount, int yAmount)
+	{
+		x = xAmount * speed;
+		y = yAmount * speed;
+	}
+};
+
 int main()
 {
 	RunConditionExample();
 	RunLoopExample();
 	RunPointerExample();
 	RunReferenceExample();
+
+	Player player;
+	player.x = 5;
+	player.Move(1, -1);
 
 	std::cin.get();
 }
