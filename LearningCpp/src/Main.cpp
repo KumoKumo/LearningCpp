9755f6b3f000db3b04676be3731fdb42931c23a5
KumoKumo <kuroryouji@gmail.com>
2021-01-13 20:12:57 +0900

    Add Decimal to Binary convert functions

--- LearningCpp/src/Main.cpp
+++ LearningCpp/src/Main.cpp
@@ -1,4 +1,5 @@
 #include "../header/Topics.h"
+#include<stack>
 
 class Player
 {
@@ -27,6 +28,61 @@ void FunctionFromMain()
 
 void FunctionFromStatic();
 
+std::stack<int> FillInStack(int decimal)
+{
+	std::stack<int> powerOfTwos;
+	int i = 0;
+	int powerOfTwo = (int)pow(2, i);
+	while (powerOfTwo <= decimal)
+	{
+		powerOfTwos.push(powerOfTwo);
+		i++;
+		powerOfTwo = (int)pow(2, i);
+	}
+
+	return powerOfTwos;
+}
+
+std::string ConvertDecimalToBinary(int decimal)
+{
+	std::stack<int> powerOfTwos = FillInStack(decimal);
+
+	std::string result = "";
+	while (!powerOfTwos.empty())
+	{
+		int amountToSubtract = powerOfTwos.top();
+		if (amountToSubtract <= decimal)
+		{
+			decimal = decimal - amountToSubtract;
+			result += "1";
+		}
+		else
+			result += "0";
+
+		powerOfTwos.pop();
+	}
+
+	return result;
+}
+
+void DecToBiFrom(int decimal)
+{
+	std::stack<int> binaryNumber;
+	while (decimal > 0)
+	{
+		binaryNumber.push(decimal % 2);
+		decimal = decimal / 2;
+	}
+
+	while (!binaryNumber.empty())
+	{
+		std::cout << binaryNumber.top();
+		binaryNumber.pop();
+	}
+
+	std::cout << std::endl;
+}
+
 int main()
 {
 	RunConditionExample();
@@ -49,5 +105,11 @@ int main()
 	RunLocalStaticExample();
 	RunLogExample();
 	Level level = Level::Level02;
+
+	Log("=======================================");
+	LOG(ConvertDecimalToBinary(100));
+	LOG(ConvertDecimalToBinary(642232264));
+	DecToBiFrom(642232264);
+	DecToBiFrom(19960108);
 	std::cin.get();
 }
